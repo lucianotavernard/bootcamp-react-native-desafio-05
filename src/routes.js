c1eabac6cf76d931c86f539a525e0a84662456ee
Luciano Tavernard <lucianotavernard@gmail.com>
2020-01-22 20:18:55 -0300

    Alterado a forma de navegação entre as páginas

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,28 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from 'react';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
+import Header from './components/Header';
+
 export default createAppContainer(
-  createSwitchNavigator(
+  createStackNavigator(
     {
       Home,
       Cart,
     },
     {
       initialRouteName: 'Home',
+      defaultNavigationOptions: {
+        headerTransparent: true,
+        header: () => <Header />,
+        cardStyle: {
+          backgroundColor: '#201d25',
+        },
+      },
     }
   )
 );
